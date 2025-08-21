cf44556a53f8c4aa5c3e2f2550506b10a1ced972
neoseven1977 <neoseven1977@gmail.com>
2025-08-22 03:01:56 +0500

    Create Theme.kt

---
+++ app/src/main/java/com/example/musicplayer/ui/theme/Theme.kt
@@ -0,0 +1,30 @@
+package com.example.musicplayer.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+private val DarkColors = darkColorScheme(
+    primary = Purple80,
+    secondary = PurpleGrey80,
+    tertiary = Pink80
+)
+
+private val LightColors = lightColorScheme(
+    primary = Purple40,
+    secondary = PurpleGrey40,
+    tertiary = Pink40
+)
+
+@Composable
+fun AppTheme(
+    darkTheme: Boolean = false,
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colorScheme = if (darkTheme) DarkColors else LightColors,
+        typography = Typography,
+        content = content
+    )
+}
